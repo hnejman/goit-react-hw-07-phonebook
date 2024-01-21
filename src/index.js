8d51a8a1bdfc143afd7490860235e5bc704a2706
hnejman <108231655+hnejman@users.noreply.github.com>
2024-01-21 20:35:01 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import './styles.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
 import { BrowserRouter } from 'react-router-dom';
-import {  overrides } from './theme/index.jsx';
 import {  ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme = {overrides} >
+    <ChakraProvider >
       <Provider store={store}>
         <BrowserRouter basename="goit-react-hw-08-phonebook">
           <App />

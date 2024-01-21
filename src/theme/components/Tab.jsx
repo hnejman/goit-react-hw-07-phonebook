28486b85010ed3b25111e5e5f4cd7d46c1c68073
hnejman <108231655+hnejman@users.noreply.github.com>
2024-01-21 20:32:37 +0100

    ...

--- src/theme/components/Tab.jsx
+++
@@ -1,8 +0,0 @@
-import { defineStyleConfig } from '@chakra-ui/react';
-
-export const Tab = defineStyleConfig({
-    baseStyle: {
-        color: '#ffff33',
-        bgColor: 'purple.500',
-    }
-})
